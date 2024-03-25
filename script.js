5ead38b06f0869cf0757beb602b59074e8aa5500
Minh <trancongminh.jvb@gmail.com>
2024-03-25 12:25:22 +0700

    update button no move

--- script.js
+++ script.js
@@ -16,14 +16,14 @@ yesBtn.addEventListener("click", () => {
   questionPrompt.style.display = "none"; // Ẩn dòng chữ khi người dùng nhấn vào nút Yes
 });
 
-noBtn.addEventListener("mouseover", () => {
-  const noBtnRect = noBtn.getBoundingClientRect();
-  const maxX = window.innerWidth - noBtnRect.width;
-  const maxY = window.innerHeight - noBtnRect.height;
+// noBtn.addEventListener("mouseover", () => {
+//   const noBtnRect = noBtn.getBoundingClientRect();
+//   const maxX = window.innerWidth - noBtnRect.width;
+//   const maxY = window.innerHeight - noBtnRect.height;
 
-  const randomX = Math.floor(Math.random() * maxX);
-  const randomY = Math.floor(Math.random() * maxY);
+//   const randomX = Math.floor(Math.random() * maxX);
+//   const randomY = Math.floor(Math.random() * maxY);
 
-  noBtn.style.left = randomX + "px";
-  noBtn.style.top = randomY + "px";
-});
+//   noBtn.style.left = randomX + "px";
+//   noBtn.style.top = randomY + "px";
+// });
